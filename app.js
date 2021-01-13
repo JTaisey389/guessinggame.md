0cdbc974a70feb239d1df69122a8a1eeb44cd99d
Jason Taisey <jdtaisey89@icloud.com>
2021-01-12 17:19:57 -0800

    added text, css and Java

---
+++ app.js
@@ -0,0 +1,68 @@
+'use strict';
+// Response 1
+var i1 = prompt('Would you like to enter my page?');
+if  (i1 == 'Yes')  {
+  var R1 = 'I grant you permission to my page where you will learn about me ';
+alert(R1);
+}
+
+else if (input == 'No') {
+var R2 = ('I am sorry to see you go!')
+alert(R2);
+}
+
+// console.log ('Would you like to enter my page?' + R1) ;
+// console.log ('I am sorry to see you go!' + R2);
+
+// Response 2
+var i2 = prompt('Do you live in Washington state?');
+if  (i2 == 'Yes')  {
+  var R3 = 'So do I, I have lived here all my life! ';
+alert(R3);
+}
+
+else if (i2 == 'No') {
+var R4 = ('Maybe you will visit Washington State one day!')
+alert(R4);
+}
+// Response 3
+var i3 = prompt('Do you have any pets?');
+if (i3 == 'Yes')  {
+  var R5 = 'So do I, I have a seven year old Golden Retriever named Jameson ';
+alert(R5);
+}
+
+else if (i3 == 'No') {
+var R6= ('That is too bad, maybe you will have pets one day.')
+alert(R6)
+}
+
+// Response 4
+var i4 = prompt('Do you live in a House?');
+if (i4 == 'Yes')  {
+  var R7 = 'So do I, My Husband and I bought our house in June of 2020. It is our first house and it is over 100 years old.';
+alert(R7);
+}
+
+else if (i4 == 'No') {
+  var R8= ('That is okay, some like to live in apartments, codominiums and other places')
+alert(R8)
+}
+
+// Response 5
+
+var i5 = prompt('Do you have a favorite candy?');
+if (i5 == 'Yes')  {
+  var R9 = 'My favorite candy is Jelly Beans ';
+alert(R9);
+}
+
+else if (i5 == 'No') {
+  var R10= ('That is alright, sweets are not for everyone.')
+alert(R10)
+}
+
+let userName = prompt ('Please enter your name')
+document.write('<h3>' + 'Welcome Guest' + '<h/3>')
+
+// document.write(R1 + R2 + R3 + R4 + R5 + R6 + R7 + R8 + R9 + R10 + i1 + i2 + i3 + i4 + i5)
