83495879bc85ba009bc3cc0d9eead3ec852dacea
Jason Taisey <jdtaisey89@icloud.com>
2021-01-15 18:08:19 -0800

    Fixed CSS

--- app2.js
+++ app2.js
@@ -22,12 +22,4 @@ alert('The correct Answers are, Boating, Kayaking, Skiing')
 alert('You got this many correct' + tries)
 question7()
 
-// Add a 7th question that has multiple possible correct answers that are stored in an array.
-// Give the user 6 attempts to guess the correct answer.
-// The guesses will end once the user guesses a correct answer or they run out of attempts.
-// Display all the possible correct answers to the user.
-// Consider using a loop of some sort for this question.
-// As a user, I would like to know my final score so that I can know how well I did.
-// Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
-
 
